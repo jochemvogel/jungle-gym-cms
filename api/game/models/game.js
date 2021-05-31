ec6696cfdf0c54e1eecd58c7004bc2fdd93cc117
Jochem Vogel <voge0031@hz.nl>
2021-05-31 14:15:52 +0200

    Update slugify name

--- api/game/models/game.js
+++ api/game/models/game.js
@@ -10,13 +10,13 @@ const slugify = require("slugify");
 module.exports = {
   lifecycles: {
     beforeCreate: async (data) => {
-      if (data.title) {
-        data.slug = slugify(data.title, { lower: true });
+      if (data.name) {
+        data.slug = slugify(data.name, { lower: true });
       }
     },
     beforeUpdate: async (params, data) => {
-      if (data.title) {
-        data.slug = slugify(data.title, { lower: true });
+      if (data.name) {
+        data.slug = slugify(data.name, { lower: true });
       }
     },
   },
