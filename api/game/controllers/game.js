0d08a1d89f277e9ab762d240ccaaae99da9e7404
Jochem Vogel <voge0031@hz.nl>
2021-06-04 12:56:31 +0200

    Update game controller

--- api/game/controllers/game.js
+++ api/game/controllers/game.js
@@ -1,8 +1,10 @@
-'use strict';
+const { sanitizeEntity } = require("strapi-utils");
 
-/**
- * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#core-controllers)
- * to customize this controller
- */
+module.exports = {
+  async findOne(ctx) {
+    const { slug } = ctx.params;
 
-module.exports = {};
+    const entity = await strapi.services.game.findOne({ slug });
+    return sanitizeEntity(entity, { model: strapi.models.game });
+  },
+};
